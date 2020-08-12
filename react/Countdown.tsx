a25fdff6403f7cb58d5b7f311d7da00d06a48891
Karolina Sena <karolina.sena16@gmail.com>
2020-08-12 12:19:22 -0300

    conectando backend e frontend

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import { TimeSplit } from './typings/global';
 import { tick } from './utils/time';
 import { useCssHandles } from 'vtex.css-handles';
-
-interface CountdownProps {
-  targetDate: string
-}
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
 
 interface CountdownProps {
   targetDate: string
@@ -15,7 +14,7 @@ const CSS_HANDLES = ['countdown'];
 
 const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
@@ -23,12 +22,45 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = D
   });
 
   const handles = useCssHandles(CSS_HANDLES);
-  tick(targetDate, setTime);
 
+  const productContext = useProduct();
+  const product = productContext?.product;
+  const linkText = product?.linkText;
+
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false
+  });
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
   return (
     <div className={`${handles.countdown} db tc`}>
-    {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-  </div>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
   )
 }
 
